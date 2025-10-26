c3c80ca0a9163c0a0d5db7ecd58eea8f7426483e
Amirhossein Lashkari <am.lashkari2017@gmail.com>
2025-10-26 21:23:05 +0330

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -128,19 +128,22 @@ dns_wordlist() {
 dns_brute_full () {
         echo "cleaning..."
         rm -f "$1.wordlist $1.dns_brute $1.dns_gen"
+        
         echo "making static wordlist..."
-        awk -v domain="$1" '{print $0"."domain}' "$WL_PATH/subdomains/assetnote-merged.txt" >> "$1.wordlist"
-        echo "making 4 chars wordlist..."
-        awk -v domain="$1" '{print $0"."domain}' "$WL_PATH/4-lower.txt" >> "$1.wordlist"
+        awk -v domain="$1" '{print $0"."domain}' "./wordlist/merged-wordlist.txt.txt" >> "$1.wordlist"
+        
         echo "shuffledns static brute-force..."
         shuffledns -list $1.wordlist -d $1 -r ~/.resolvers -m $(which massdns) -mode resolve -t 30 -silent | tee $1.dns_brute 2>&1 > /dev/null
         echo "[+] finished, total $(wc -l $1.dns_brute) resolved..."
+        
         echo "running subfinder..."
-        subfinder -d $1 -all | dnsx -silent | anew $1.dns_brute 2>&1 > /dev/null
+        subfinder -d $1 -all | dnsx -silent | anew $1.dns_brute 2>&1 > /dev/null 
         echo "[+] finished, total $(wc -l $1.dns_brute) resolved..."
+        
         echo "running DNSGen..."
-        cat $1.dns_brute | dnsgen -w $WL_PATH/subdomains/words.txt - > $1.dns_gen 2>&1 > /dev/null
+        cat $1.dns_brute | dnsgen -w ./wordlist/dynamic.txt - > $1.dns_gen 2>&1 > /dev/null
         echo "finished with $(wc -l $1.dns_gen) words..."
+        
         echo "shuffledns dynamic brute-force on dnsgen results..."
         shuffledns -list $1.dns_gen -d $1 -r ~/.resolvers -m $(which massdns) -mode resolve -t 30 -silent | anew $1.dns_brute 2>&1 > /dev/null
         echo "[+] finished, total $(wc -l $1.dns_brute) resolved..."
